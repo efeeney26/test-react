9a14a10d1a80a4319fa419bcc0c7cfebc1345740
Kochkin Danila <>
2021-01-14 18:51:16 +0300

    cypress unit tests

--- cypress/component/Home.spec.jsx
+++ cypress/component/Home.spec.jsx
@@ -6,5 +6,12 @@ describe('Home', () => {
   it('render link', () => {
     mount(<Home />)
     cy.contains('Hello').should('be.visible')
+    cy.contains('Description').should('be.visible')
+
+    cy.get('button')
+      .contains('Button')
+      .click()
+
+    cy.contains('Tooltip')
   })
 })
