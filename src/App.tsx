f7f60b7e00e3433acc55d7aa2991ff7393a7c1c6
Kochkin Danila <>
2021-05-16 17:18:00 +0300

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {string} from "prop-types";
+
+const test = <Type extends { toString: Function }>(
+    arr: Type[]
+): Type => {
+  return arr[0].toString()
+}
+
+interface Test {
+  id: number,
+  name: string
+}
+
+const test1 = <Type extends { name: string }>(arr: Array<Type>, key: string): Type | undefined => arr.find((item) => item.name === key)
 
 function App () {
+  console.log(test<string | number>([1, 2]))
+  console.log(test1([{id: 1, name: 'lol'}, {id: 2, name: 'wow'}], 'wow'))
   return (
     <div className="App">
       <header className="App-header">
